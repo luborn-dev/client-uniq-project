9a93d7b23a266e14c45fb610114e76bc7d553014
luborn-dev <luborn.dev@gmail.com>
2022-11-10 21:46:04 -0300

    feat: working client side

--- app/src/main/java/br/com/uniq/Main.java
+++ app/src/main/java/br/com/uniq/Main.java
@@ -5,7 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.URL;
 
 public class Main extends Application {
@@ -20,6 +24,38 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        // launch();
+        // UNCOMMENT THE LINE ABOVE TO RUN JAVA.FX
+
+        // BELOW CODE IS JUST TESTING THE CLIENT-SERVER
+        try
+        {
+            Socket conexao = new Socket ("localhost",7777);
+            PrintWriter transmissor =
+                    new PrintWriter (
+                            conexao.getOutputStream ());
+
+            BufferedReader teclado =
+                    new BufferedReader (
+                            new InputStreamReader(
+                                    System.in));
+
+            String texto;
+
+            do
+            {
+                texto = teclado.readLine ();
+                transmissor.println (texto);
+                transmissor.flush ();
+            }
+            while (!texto.equalsIgnoreCase("FIM"));
+
+            transmissor.close();
+            conexao.close();
+        }
+        catch (Exception erro)
+        {
+            System.err.println (erro.getMessage());
+        }
     }
 }
